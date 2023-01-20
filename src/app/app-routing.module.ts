18f58388295bea77fb5714e955a185b477cb568e
Surber Fabian <fabian.surber@bbcag.ch>
2023-01-20 17:53:34 +0100

    solid menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {TechnischeDatenComponent} from "./technische-daten/technische-daten.component";
+import {FeaturesComponent} from "./features/features.component";
+import {KaufenComponent} from "./kaufen/kaufen.component";
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'technische-daten', component: TechnischeDatenComponent},
+  { path: 'features', component: FeaturesComponent},
+  { path: 'kaufen', component: KaufenComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TechnischeDatenComponent } from './technische-daten/technische-daten.component';
+import { FeaturesComponent } from './features/features.component';
+import { KaufenComponent } from './kaufen/kaufen.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TechnischeDatenComponent,
+    FeaturesComponent,
+    KaufenComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
